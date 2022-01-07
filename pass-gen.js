2eb34bcf8720c8ed1b325965574b3842015f1cc7
bolivarlagos <bolivarlagos@gmail.com>
2022-01-07 14:33:26 -0300

    writing and displaying

--- pass-gen.js
+++ pass-gen.js
@@ -1,14 +1,15 @@
 #!/usr/bin/env node
 
 const { program, Option } = require("commander")
+const { writeFile } = require("fs")
 const { version } = require("./package.json")
 const createPassword  = require("./passwordHandler")
 
-const securityOption = new Option("-s, --sec <security>", "Define the password security")
-
 program.version(version, "-v, --version", "Display the current version of the program")
 
-program.addOption(securityOption.default("medium").choices(["weak", "medium", "strong"]))
+const securityOption = new Option("-s, --sec <security>", "Define the password security").default("medium")
+
+program.addOption(securityOption.choices(["weak", "medium", "strong"]))
 
 program
     .option("-l, --length <number>", "Password size", "8")
@@ -17,7 +18,7 @@ program
     .option("-w, --write", "Save in a text file", false)
 
 program
-    .option("-d, --display", "Display the password in the console", false)
+    .option("-d, --display", "Display the password in the console", true)
 
 program.parse()
 
@@ -25,5 +26,16 @@ let { sec, length, write, display } = program.opts()
 
 const generatedPassword = createPassword(sec, length)
 
-console.log(generatedPassword.length)
-console.log(generatedPassword)
+if(display){
+    console.log("This is your password ", generatedPassword)
+}
+
+if(write){
+    writeFile("password.txt", generatedPassword, (err) => {
+        if(err){
+            console.log(err)
+        }
+        console.log("Success write")
+    })
+}
+
